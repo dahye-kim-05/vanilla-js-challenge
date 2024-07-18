e4f787727bb975019ff92f91715c6da8d834768c
dahye-kim-05 <kimda0515@naver.com>
2024-07-18 17:29:14 +0900

    code challenge_1
    how to access 'HTML in JavaScript' and use 'function', 'eventListening', and several 'events'.

--- whatever.js
+++ whatever.js
@@ -1,8 +1,29 @@
-const title = document.querySelector(".hello h1:first-child");
+const title = document.querySelector("div.hello h1:first-child");
 
 function handleClickevent() {
   title.style.color = "blue";
 }
 
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone";
+}
+
+function handleMouseRight() {
+  title.innerText = "Menu";
+  title.style.color = "brown";
+}
+
+function handleWindowResize(){
+  title.style.color = "tomato";
+}
+
 title.addEventListener("click", handleClickevent);
-// event = listner function 기능들. (ex. click, submit, etc..)
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+title.addEventListener("contextmenu", handleMouseRight);
+
+window.addEventListener("resize", handleWindowResize);
