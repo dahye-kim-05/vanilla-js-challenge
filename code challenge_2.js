5dffdf8101e9683c2df881a60af617705c0266e4
dahye-kim-05 <kimda0515@naver.com>
2024-07-21 02:06:50 +0900

    code challenge_2
    nomad coder's challenge_2

--- code challenge_2.js
+++ code challenge_2.js
@@ -1,15 +1,25 @@
 const widthPicxel = document.querySelector("#width");
-document.body.style.backgroundColor = "purple";
+const body = document.body;
+// html 의 body 태그를 js에서 사용할 수 있도록 선언해 줌.
+
+const big_Screen = "bigScreen";
+const medium_Screen = "mediumScreen";
+const small_Screen = "smallScreen";
 
 function handleWindowSize() {
   widthPicxel.textContent = window.innerWidth;
   const width = window.innerWidth;
-  if (width >= 1000) {
-    document.body.style.backgroundColor = "purple";
-  } else if (1000 > width && width >= 800) {
-    document.body.style.backgroundColor = "yellow";
-  } else if (800 > width) {
-    document.body.style.backgroundColor = "brown";
+  if (width > 1000) {
+    body.classList.add(big_Screen);
+    body.classList.remove(medium_Screen);
+  } else if (width <= 1000 && width >= 800) {
+    body.classList.add(medium_Screen);
+    body.classList.remove(big_Screen, small_Screen);
+    // 이 때,  remove 메서드에 'small_Screen' 도 추가해야 함. 
+    // 지워주지 않는다면, 브라우저 창 size 늘렸을 때 small 메서드가 지속됨.
+  } else {
+    body.classList.remove(medium_Screen);
+    body.classList.add(small_Screen);
   }
 }
 
